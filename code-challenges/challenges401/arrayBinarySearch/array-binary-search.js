6cf365b819a2eb400d64d08224cbebdd3a466ce7
dinaAlsaid <dinaAlsaid@github.com>
2020-10-13 23:59:36 +0300

    tests added

--- code-challenges/challenges401/arrayBinarySearch/array-binary-search.js
+++ code-challenges/challenges401/arrayBinarySearch/array-binary-search.js
@@ -1,21 +1,21 @@
 'use strict';
 function binarySearch(arr,key){
     let iFirst = 0;
-    let iLast = arr.length;
+    let iLast = arr.length-1;
     let mid = Math.ceil((iLast-iFirst)/2);
-    
-    while((iLast-iFirst) !== 0){
+
+    while(iLast >= iFirst){
         if (key == arr[mid]){
             return key;
-        }else if(key <= arr[mid-1]){
-            iLast=mid-1;
-        }else if (key >= arr[mid+1]){
+        }else if(key < arr[mid]){
+            iLast= mid-1;
+        }else if (key > arr[mid]){
             iFirst = mid +1;
-        } else {
-            return -1;
-        }
-        mid = Math.ceil((iLast-iFirst)/2)
+        } 
+        mid =iFirst+ Math.ceil((iLast-iFirst)/2)
+
     }
+    return -1;
 }
 
 module.exports = binarySearch;
