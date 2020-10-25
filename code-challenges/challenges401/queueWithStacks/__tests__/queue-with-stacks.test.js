915cb708f4d6134b07c0d912cb8c1103b1693623
dinaAlsaid <dinaAlsaid@github.com>
2020-10-25 23:29:49 +0300

    code

---
+++ code-challenges/challenges401/queueWithStacks/__tests__/queue-with-stacks.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const psuedoQ = require('../queue-with-stacks.js');
+
+describe('Psuedo Queue',()=>{
+  const newQ = new psuedoQ();
+  it('Performs an exception when empty',()=>{
+    expect()
+  })
+})
--- code-challenges/challenges401/queueWithStacks/queue-with-stacks.js
+++ code-challenges/challenges401/queueWithStacks/queue-with-stacks.js
@@ -4,23 +4,29 @@ const SandQ = require('../../Data-structures/stacksAndQueues/stacks-and-queues;'
 
 class PsuedoQueue{
   constructor(){
+    this.front = null;
+    this.rear =null;
     this.stack1 = new SandQ.stack();
     this.stack2 = new SandQ.stack();
   }
 
   enqueue(val){
+    if(!this.stack1.top){
+      this.front=this.stack1.top;
+    }
     this.stack1.push(val);
+    this.rear = this.stack1.top; 
   }
 
   dequeue(){
     if(this.stack1.peek() === 'empty'){
       return 'empty';
     }else{
-      while(!this.stack1.top.next){
+      while(this.stack1.top.next){
         this.stack2.push(this.stack1.pop());
       }
       let temp = this.stack1.pop();
-      while(!this.stack2.top){
+      while(this.stack2.top){
         this.stack1.push(this.stack2.pop());
       }
       return temp;
