86d03d9846516d483650cb3532af3c1e38ae8236
dinaAlsaid <dinaAlsaid@github.com>
2020-11-17 01:04:37 +0200

    merge sort

---
+++ code-challenges/challenges401/Sort/merge/__tests__/mergeSort.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+'use strict';
+
+const mergeSort = require('../mergeSort.js');
+
+describe('Selection sorting algorithm', () => {
+  it('returns a sorted array', () => {
+    expect(mergeSort([8, 4, 23, 42, 16, 15])).toEqual([4, 8, 15, 16, 23, 42]);
+  });
+  it('returns an array of one element',()=>{
+    expect(mergeSort([8])).toEqual([8]);
+
+  });
+  it('handles an empty array for an input',()=>{
+    expect(mergeSort([])).toEqual('empty');
+
+  });
+
+});
