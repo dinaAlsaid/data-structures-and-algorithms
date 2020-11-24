abbb7e806a5d89bd58aacd4edf3e473aa88516e1
dinaAlsaid <dinaAlsaid@github.com>
2020-11-24 12:11:47 +0200

    fix the tree tests add method is working

---
+++ code-challenges/challenges401/treeInsertion/__tests__/tree-insertion.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const {BinaryTree} = require('../../Data-structures/tree/tree.js');
+const treeInsertion = require('../tree-insertion.js');
+
+const BT1 = new BinaryTree();
+const BT2 = new BinaryTree();
+BT1.add(1);
+BT1.add(3);
+BT1.add(2);
+
